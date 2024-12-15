b30c8e692b7bb96d32317262276b9b679a5c0677
2sseul <iseul0626@gmail.com>
2024-12-16 00:11:52 +0900

    unit3 -ing

--- unit32/app/src/main/java/Basics.kt
+++ unit32/app/src/main/java/Basics.kt
@@ -1,13 +1,16 @@
 fun main() {
-    makeCoffee(1)
-    makeCoffee(4)
-    makeCoffee(-1)
+    val name = readln()
+    val sugarCount = readln().toInt()
+    makeCoffee(name, sugarCount)
 }
 
-fun makeCoffee(sugarCount: Int){
+fun makeCoffee(name: String ,sugarCount: Int){
     if(sugarCount == 1){
-        println("Coffee with $sugarCount spoon of sugar")
-    }else{
-        println("Coffee with $sugarCount spoons of sugar")
+        println("Coffee with $sugarCount spoon of sugar for $name")
+    }else if(sugarCount == 0){
+        println("Coffee with no sugar for $name")
+    }
+    else{
+        println("Coffee with $sugarCount spoons of sugar for $name")
     }
 }
